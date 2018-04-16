c9bc6a85bceb239db5ad4dfb313639980df5463c
Madhukesh Ayyagari <mayyagari7@gatech.edu>
2018-04-16 17:17:03 -0400

    adding ppm.cpp and ppm.h and process

--- 2018-MUSI6106-assignment_5/src/inc/Ppm.h
+++ 2018-MUSI6106-assignment_5/src/inc/Ppm.h
@@ -2,20 +2,30 @@
 #define __Ppm_hdr__
 
 #include "ErrorDef.h"
+#include "math.h"
 
 class CPpm
 {
 public:
     static Error_t createInstance (CPpm*& pCPpm);
     static Error_t destroyInstance (CPpm*& pCPpm);
-
-    Error_t process (float **ppfInputBuffer, int iNumberOfFrames);
+	Error_t CPpm::initInstance(float fSampleRateInHz, int iNumChannels);
+    Error_t process (float **ppfInputBuffer, float *ppfOutputBuff, int iNumberOfFrames);
 
 protected:
     CPpm ();
     virtual ~CPpm ();
 
 private:
+	float m_fSampleRate;
+	int m_iNumChannels;
+	float m_AlphaAT;
+	float m_AlphaRT;
+	float m_epsilon;
+	float m_currentValue;
+
+	float *m_tempBuff;
+	float *m_vppmMax;
 
 };
 
