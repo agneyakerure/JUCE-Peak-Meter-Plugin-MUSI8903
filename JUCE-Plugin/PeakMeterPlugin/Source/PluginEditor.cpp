55c051599e26b2d7e6969560b8ff0ec1e73524f0
Yi Wu <ywu663@gatech.edu>
2018-04-17 17:18:32 -0400

    Display Issue Fixed

--- JUCE-Plugin/PeakMeterPlugin/Source/PluginEditor.cpp
+++ JUCE-Plugin/PeakMeterPlugin/Source/PluginEditor.cpp
@@ -11,6 +11,9 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
+#include <iostream>
+using namespace std;
+
 //==============================================================================
 PeakMeterPluginAudioProcessorEditor::PeakMeterPluginAudioProcessorEditor (PeakMeterPluginAudioProcessor& p)
     : AudioProcessorEditor (&p), processor (p)
@@ -36,21 +39,26 @@ void PeakMeterPluginAudioProcessorEditor::paint (Graphics& g)
 
     g.setColour (Colours::white);
     g.setFont (15.0f);
+
 }
 
 void PeakMeterPluginAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
-    const int border = 120;
-    m_pPeakMeterCh1.setBounds(border, 20, getWidth() - border, 20);
-    m_pPeakMeterCh2.setBounds(border, 20, getWidth() - border, 50);
+//    const int border = 120;
+//    m_pPeakMeterCh1.setBounds(border, 20, getWidth() - border, 20);
+//    m_pPeakMeterCh2.setBounds(border, 20, getWidth() - border, 50);
     
+    m_pPeakMeterCh1.setBounds(getWidth()/2 - 150, 50, 20, 150);
+    m_pPeakMeterCh2.setBounds(getWidth()/2 - 120, 50, 20, 150);
     
 }
 
 void PeakMeterPluginAudioProcessorEditor::timerCallback() {
     m_pPeakMeterCh1.setPMValue(processor.getMaxPeakMeterValue(0));
     m_pPeakMeterCh2.setPMValue(processor.getMaxPeakMeterValue(1));
+    
+    
 }
 
